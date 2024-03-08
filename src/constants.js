46f98a01523c9628ac606be605012bd6b61b1cf0
tfkcoder <lucianojackson048@gmail.com>
2024-03-08 11:26:43 +0300

    change api

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 export const drawerWidth = 250;
 export const APP_NAME = "Customer-Billing";
 // export const BASE_URL = "http://192.168.150.130:4000/";
-// export const BASE_URL = "http://64.23.182.248/";
-export const BASE_URL = "http://localhost:4000/";
+export const BASE_URL = "http://64.23.182.248/";
+// export const BASE_URL = "http://localhost:4000/";
 
 // "proxy": "http://88.198.102.80:30003/",
 //     "homepage": "http://88.198.102.80:3005"
